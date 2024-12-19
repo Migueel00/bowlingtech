69672eb3b1fe8da9b2fe88da227b3851707f7bfe
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-12-19 10:30:00 +0100

    [MAIN][NEW]Map iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,16 @@ export default function Home() {
       </div>
       <Hero />
       <Container>
+        <div>
+          <h2 className="text-2xl">
+            Ven a vernos C/Portuetxe 88, 20018, San Sebastian
+          </h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.609412389046!2d-2.0229302234356297!3d43.30150067510615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd51b064da0df555%3A0xb1aa6421bdaa58e1!2sC.%20Portuetxe%2C%2088%2C%2020018%20San%20Sebasti%C3%A1n%2C%20Guip%C3%BAzcoa!5e0!3m2!1ses!2ses!4v1734598481862!5m2!1ses!2ses" 
+            className="w-[45%] h-[450px] mt-10"
+            />
+        </div>
+      </Container>
+      <Container>
         <div className="mt-10">
           <div className="flex justify-center">
             <div className="pt-2 text-gray-400 dark:text-gray-400 w-1/4 flex justify-center">
